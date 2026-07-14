8c22b3dd04cfd7403c8ac36ca176bbccfd55d029
Prateek Agrahari <141808871+Prateek-1110@users.noreply.github.com>
2026-07-14 09:15:05 +0530

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& v) {
+           int n = v.size();
+        vector<int> dp(n);
+
+        dp[0] = v[0];
+        dp[1] = v[1];
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = v[i] + min(dp[i - 1], dp[i - 2]);
+        }
+
+        return min(dp[n - 1], dp[n - 2]);
+    }
+};
